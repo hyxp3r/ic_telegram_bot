ef081b6f8f6c1bdc6aa180b23836b5678e98e98f
Evgeniy <erma2001@mail.ru>
2024-02-28 14:03:39 +0700

    refactor urls

--- bot/keybords/inline_keyboards/ic_inline.py
+++ bot/keybords/inline_keyboards/ic_inline.py
@@ -6,6 +6,8 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from bot.static.text.buttons.home_buttons import (
                                                     IC_SITE,
+                                                    IC_SITE_URL,
+                                                    IC_SITE_GROUP,
                                                     IC_GROUP,
                                                     FINANCE,
                                                     ROOT
@@ -22,11 +24,11 @@ def build_ic_kb() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(
         text=IC_SITE,
-        url="https://ic.nsuem.ru/"
+        url=IC_SITE_URL
     )
     builder.button(
         text=IC_GROUP,
-        url="https://vk.com/nsuem_ic"
+        url=IC_SITE_GROUP
     )
     builder.button(
         text=FINANCE,
--- bot/static/text/buttons/home_buttons.py
+++ bot/static/text/buttons/home_buttons.py
@@ -3,3 +3,6 @@ IC_GROUP = "🏘️ Группа ВК"
 FINANCE = "💸 Проверить фин. задолженность"
 
 ROOT = "🏠 На главную"
+
+IC_SITE_URL = "https://ic.nsuem.ru/"
+IC_SITE_GROUP = "https://vk.com/nsuem_ic"
